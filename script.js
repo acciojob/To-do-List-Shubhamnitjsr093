b277c0def422ea1aaf643733294aaf0531d1c2c4
Shubhamnitjsr093 <99527655+Shubhamnitjsr093@users.noreply.github.com>
2023-12-22 21:00:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+addTodoBtn.addEventListener("click", () => {
+  const todoText = newTodoInput.value.trim();
+
+  if (todoText !== "") {
+    const newTodoListItem = document.createElement("li");
+    newTodoListItem.textContent = todoText;
+    todoList.appendChild(newTodoListItem);
+    newTodoInput.value = ""; // Clear the input field
+  }
+});
